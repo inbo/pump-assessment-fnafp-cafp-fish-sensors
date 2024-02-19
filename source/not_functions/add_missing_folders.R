365d4b9275145fd33b4fbe414674574ef5ca4ba3
BRUNEEL <stijn.bruneel@inbo.be>
2024-02-19 16:24:33 +0100

    remove internal data; should not be pushed

--- source/not_functions/add_missing_folders.R
+++ source/not_functions/add_missing_folders.R
@@ -1,5 +1,6 @@
 if (file.exists("./data/internal/")==FALSE){dir.create("./data/internal/")}
 if (file.exists("./data/internal/power/")==FALSE){dir.create("./data/internal/power/")}
+if (file.exists("./data/internal/NP/")==FALSE){dir.create("./data/internal/NP/")}
 if (file.exists("./figures/")==FALSE){dir.create("./figures/")}
 if (file.exists("./figures/additional/")==FALSE){dir.create("./figures/additional/")}
 if (file.exists("./figures/manuscript/")==FALSE){dir.create("./figures/manuscript/")}

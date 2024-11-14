057c81dfed93c199fd6860b120958cb76a026ede
Stijn Bruneel <109136281+stijnbruneel@users.noreply.github.com>
2024-11-14 13:50:56 +0100

    Update add_missing_folders.R

--- source/not_functions/add_missing_folders.R
+++ source/not_functions/add_missing_folders.R
@@ -1,7 +1,11 @@
-if (file.exists("./data/fish/internal/")==FALSE){dir.create("./data/internal/")}
-if (file.exists("./data/fish/internal/power/")==FALSE){dir.create("./data/internal/power/")}
-if (file.exists("./data/fish/internal/NP/")==FALSE){dir.create("./data/internal/NP/")}
-if (file.exists("./data/fish/internal/additional/")==FALSE){dir.create("./data/internal/additional/")}
+if (file.exists("./data/fish/internal/")==FALSE){dir.create("./data/fish/internal/")}
+if (file.exists("./data/fish/internal/power/")==FALSE){dir.create("./data/fish/internal/power/")}
+if (file.exists("./data/fish/internal/NP/")==FALSE){dir.create("./data/fish/internal/NP/")}
+if (file.exists("./data/fish/internal/additional/")==FALSE){dir.create("./data/fish/internal/additional/")}
 if (file.exists("./figures/")==FALSE){dir.create("./figures/")}
 if (file.exists("./figures/additional/")==FALSE){dir.create("./figures/additional/")}
 if (file.exists("./figures/manuscript/")==FALSE){dir.create("./figures/manuscript/")}
+if (file.exists("./data/bds/")==FALSE){dir.create("./data/bds/")}
+if (file.exists("./data/bds/external/")==FALSE){dir.create("./data/bds/external/")}
+if (file.exists("./data/bds/internal/")==FALSE){dir.create("./data/bds/internal/")}
+
